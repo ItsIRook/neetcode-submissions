6d04e3258d8c359b81ac2f221ccfb58155952ff1
Amarnath K <itsrook2005@gmail.com>
2026-07-30 23:25:43 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> freq;
+        for(char c : s) freq[c]++;
+        for(char c : t) freq[c]--;
+        for(auto [ch, count] : freq) if(count!=0) return false;
+        return true;
+    }
+};
